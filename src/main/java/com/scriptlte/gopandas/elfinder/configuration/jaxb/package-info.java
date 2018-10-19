63e095a4f39113508b5140c8cec941559f879c20
bopresslte <34298824@qq.com>
2018-10-19 11:29:43 +0800

    init.

---
+++ src/main/java/com/scriptlte/gopandas/elfinder/configuration/jaxb/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.trustsystems.com.br/elfinder", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.scriptlte.gopandas.elfinder.configuration.jaxb;

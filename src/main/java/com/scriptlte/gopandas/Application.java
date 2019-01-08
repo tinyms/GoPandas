e72d1ea07d8464aee37dd9c96956183234ee49a8
xzeng <806019582@11.com>
2019-01-09 01:42:40 +0800

    简单权限添加

--- src/main/java/com/scriptlte/gopandas/Application.java
+++ src/main/java/com/scriptlte/gopandas/Application.java
@@ -19,6 +19,13 @@ import com.scriptlte.gopandas.models.Employee;
 import com.scriptlte.gopandas.models.EmployeeRepository;
 import com.scriptlte.gopandas.models.Item;
 import com.scriptlte.gopandas.models.ItemRepository;
+import com.scriptlte.gopandas.security.config.SecurityConstant;
+import com.scriptlte.gopandas.security.dao.OrgRoleRepository;
+import com.scriptlte.gopandas.security.dao.OrgUserRepository;
+import com.scriptlte.gopandas.security.pojo.OrgRole;
+import com.scriptlte.gopandas.security.pojo.OrgUser;
+import com.scriptlte.gopandas.security.service.OrgRoleService;
+import com.scriptlte.gopandas.security.service.OrgUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -37,6 +44,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This example shows various ways to secure Spring Data REST applications using Spring Security
@@ -44,7 +53,6 @@ import javax.annotation.PostConstruct;
  * @author Greg Turnquist
  */
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
@@ -53,6 +61,14 @@ public class Application {
 	@Autowired
     EmployeeRepository employeeRepository;
 
+	@Autowired
+	OrgUserService orgUserService;
+	@Autowired
+	OrgRoleService orgRoleService;
+	@Autowired
+	OrgUserRepository orgUserRepository;
+	@Autowired
+	OrgRoleRepository orgRoleRepository;
 	public static void main(String[] args) {
 	    SpringApplication.run(Application.class);
 	}
@@ -61,73 +77,46 @@ public class Application {
 	 * Pre-load the system with employees and items.
 	 */
 	public @PostConstruct void init() {
-
-		employeeRepository.save(new Employee("Bilbo", "Baggins", "thief"));
-		employeeRepository.save(new Employee("Frodo", "Baggins", "ring bearer"));
-		employeeRepository.save(new Employee("Gandalf", "the Wizard", "servant of the Secret Fire"));
+		addUserRole();
+//		employeeRepository.save(new Employee("Bilbo", "Baggins", "thief"));
+//		employeeRepository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+//		employeeRepository.save(new Employee("Gandalf", "the Wizard", "servant of the Secret Fire"));
 
 		/**
 		 * Due to method-level protections on {@link example.company.ItemRepository}, the security context must be loaded
 		 * with an authentication token containing the necessary privileges.
 		 */
-		SecurityUtils.runAs("system", "system", "ROLE_ADMIN");
+//		SecurityUtils.runAs("system", "system", "ROLE_ADMIN");
 
-		itemRepository.save(new Item("Sting"));
-		itemRepository.save(new Item("the one ring"));
+//		itemRepository.save(new Item("Sting"));
+//		itemRepository.save(new Item("the one ring"));
 
-		SecurityContextHolder.clearContext();
 	}
 
-	/**
-	 * This application is secured at both the URL level for some parts, and the method level for other parts. The URL
-	 * security is shown inside this code, while method-level annotations are enabled at by
-	 * {@link EnableGlobalMethodSecurity}.
-	 *
-	 * @author Greg Turnquist
-	 * @author Oliver Gierke
-	 */
-	@Configuration
-	@EnableGlobalMethodSecurity(prePostEnabled = true)
-	@EnableWebSecurity
-	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-		/**
-		 * This section defines the user accounts which can be used for authentication as well as the roles each user has.
-		 */
-		@Bean
-		InMemoryUserDetailsManager userDetailsManager() {
-
-			UserBuilder builder = User.withDefaultPasswordEncoder();
-
-			UserDetails greg = builder.username("greg").password("turnquist").roles("USER").build();
-			UserDetails ollie = builder.username("ollie").password("gierke").roles("USER", "ADMIN").build();
-
-			return new InMemoryUserDetailsManager(greg, ollie);
+	public void addUserRole(){
+		OrgUser userAdmin = orgUserService.getUserByUserName("admin");
+		if (userAdmin==null)
+			userAdmin = new OrgUser();
+		userAdmin.setUsername("admin");
+		userAdmin.setPassword("123");
+		userAdmin.setStatus(SecurityConstant.USER_STATUS_ENABLE);
+		orgUserService.saveOrUpdate(userAdmin);
+		OrgUser user = orgUserService.getUserByUserName("zhangsan");
+		if (user == null)
+			user = new OrgUser();
+		user.setUsername("zhangsan");
+		user.setPassword("123");
+		user.setStatus(SecurityConstant.USER_STATUS_ENABLE);
+		orgUserService.saveOrUpdate(user);
+		OrgRole role = orgRoleService.getRoleByName("角色名");
+		if (role == null){
+			role = new OrgRole();
 		}
-
-		/**
-		 * This section defines the security policy for the app.
-		 * <p>
-		 * <ul>
-		 * <li>BASIC authentication is supported (enough for this REST-based demo).</li>
-		 * <li>/employees is secured using URL security shown below.</li>
-		 * <li>CSRF headers are disabled since we are only testing the REST interface, not a web one.</li>
-		 * </ul>
-		 * NOTE: GET is not shown which defaults to permitted.
-		 *
-		 * @param http
-		 * @throws Exception
-		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
-		 */
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-
-			http.httpBasic().and().authorizeRequests().//
-                    antMatchers(HttpMethod.GET, "/employees").hasRole("ADMIN").//
-					antMatchers(HttpMethod.POST, "/employees").hasRole("ADMIN").//
-					antMatchers(HttpMethod.PUT, "/employees/**").hasRole("ADMIN").//
-					antMatchers(HttpMethod.PATCH, "/employees/**").hasRole("ADMIN").and().//
-					csrf().disable();
-		}
+		List<OrgRole> roles = new ArrayList<>();
+		role.setRoleName("角色名");
+		orgRoleService.save(role);
+		roles.add(role);
+		user.setRoles(roles);
+		orgUserService.saveOrUpdate(user);
 	}
 }
--- src/main/java/com/scriptlte/gopandas/security/pojo/OrgUser.java
+++ src/main/java/com/scriptlte/gopandas/security/pojo/OrgUser.java
@@ -1,40 +1,40 @@
 package com.scriptlte.gopandas.security.pojo;
 
 import com.scriptlte.gopandas.security.config.SecurityConstant;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Data
 public class OrgUser implements UserDetails, Serializable {
+
     private static final long serialVersionUID = 6427512514997356L;
 
     @Id
     @GeneratedValue
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
     private String status;
-    @ManyToMany(targetEntity = OrgRole.class)
+    @ManyToMany(targetEntity = OrgRole.class,fetch = FetchType.EAGER)
     private List<OrgRole> roles;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
+        List<GrantedAuthority> auths = new ArrayList<>();
+        for (OrgRole role : roles){
+            auths.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return auths;
     }
 
     @Override
